810a8dc94799cea2d883306f682f4d3430472821
Prayankag <131856399+Prayankag@users.noreply.github.com>
2025-01-06 17:05:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,10 @@ data "azurerm_resource_group" "example" {
 }
 provider "azurerm" {
   features {}
-  
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret  = var.client_secret
+  tenant_id      = var.tenant_id
 }
 
 # Use environment-specific values dynamically
